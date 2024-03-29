f68af7985da64d5dd1e845c86c6abdf5d66ce7c8
hanhan0901 <11160826@me.mcu.edu.tw>
2024-03-29 11:55:36 +0800

    week06

---
+++ week06/week06-1a.cpp
@@ -0,0 +1,10 @@
+///畫星星,從9X9乘法表開始
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){///左手i
+        printf("i:%d\n",i);
+    }
+}
---
+++ week06/week06-1b.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){///¥ª¤âi
+        for(int j=1;j<=n;j++){///¥k¤âi
+            printf("*");
+
+        }
+        printf("i:%d\n",i);
+    }
+}
